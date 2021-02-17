f6d6dfa381cb2cfd9f3bdfa5597b03c3958c203c
patcasandrei <patcas_andrei@yahoo.ro>
2021-02-17 21:35:42 +0200

    test1

---
+++ modul1/modul1_1.py
@@ -0,0 +1,17 @@
+# print ('print\u200b("")')
+# # print​("")
+# print ('Patcas Andrei')
+# print ('Numele meu este:','Andrei','1',sep='_',end='*')
+# print ('Numele meu este:','Andrei','2',sep='_',end='*')
+# print ('Numele meu este:','Andrei','3',sep='_',end='*')
+# print ()
+# print_text1='Hello World'
+# print_text2='Hello world 2'
+# print (print_text1 ,print_text2)
+# print_text1= 123
+# print_text2=456
+# print (print_text1,print_text2)
+print("","","","*")
+print("","","***")
+print("","*****")
+print("*******")
