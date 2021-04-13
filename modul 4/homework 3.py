6bb079ceacb8816aaea6f2ed2125c89b2a397062
patcasandrei <patcas_andrei@yahoo.ro>
2021-04-13 10:33:11 +0300

    homework 6.py

--- modul 4/homework 3.py
+++ modul 4/homework 3.py
@@ -1,87 +1,87 @@
-# #
-# #
-# # # 25P
-# # # Write a function that takes in a list of objects and converts each object in the list into a int.
-# # # For objects that can't be directly converted to int should have their length counted
-# # # The function will return a list with a int values ordered from largest to smallest.
-# # # example [1, True, '123', False, 6, ()] will be transformed into [123, 6, 1, 1, 0, 0]
-# #
-# order_list=[]
-# def ordered_ints(list_of_objects: list):
-#     for obj in list_of_objects:
-#         if type(obj) is int:
-#             order_list.append(obj)
-#         elif type(obj) is bool:
-#             bool_obj=int(obj==True)
-#             order_list.append(bool_obj)
-#         elif type(obj) is str:
-#             str_obj=int(obj)
-#             order_list.append(str_obj)
-#         elif type(obj) is tuple:
-#             tuple_obj=0
-#             order_list.append(tuple_obj)
-#
-#     print(sorted(order_list,reverse=1))
-#
-# print(ordered_ints([1, True, '123', False, 6, ()]))
-# #
-# #
-# # # 25P - (do not rush to resolve this)
-# # # For recursive functions try reading the articles below if you find need more information
-# # # https://realpython.com/python-thinking-recursively/
-# # # https://www.python-course.eu/python3_recursive_functions.php
-# # # After reading the above articles try creating a function to calculate the series (1^2)+(2^2)+(3^2)...(n^2)
-# # # The function will receive an int that indicate the number of iterations, or how many times we have (x^2)+
-# # # when resolving try using this logic: 1^2+2^2 is 1^2+(1^2+1^2)^2
-# #
-# # def sum_of_square(n: int):
-# #     pass
-# #     # <your code here>
-# #
-# #
-# # print(sum_of_square(10))
-# #
-# #
-# # # 25P
-# # # Write a function that will calculate factorial of numbers squared.
-# # # # For n = 3 the function should calculate (1^2)*(2^2)*(3^2)
-#
-# def factorial_of_squares(n: int):
-#     if n==0:
-#         return 1
-#     else:
-#         resoult=n*factorial_of_squares(n-1)
-#         print(resoult)
 #
 #
-# print(factorial_of_squares(5))
-# # # #
+# # 25P
+# # Write a function that takes in a list of objects and converts each object in the list into a int.
+# # For objects that can't be directly converted to int should have their length counted
+# # The function will return a list with a int values ordered from largest to smallest.
+# # example [1, True, '123', False, 6, ()] will be transformed into [123, 6, 1, 1, 0, 0]
+#
+order_list=[]
+def ordered_ints(list_of_objects: list):
+    for obj in list_of_objects:
+        if type(obj) is int:
+            order_list.append(obj)
+        elif type(obj) is bool:
+            bool_obj=int(obj==True)
+            order_list.append(bool_obj)
+        elif type(obj) is str:
+            str_obj=int(obj)
+            order_list.append(str_obj)
+        elif type(obj) is tuple:
+            tuple_obj=0
+            order_list.append(tuple_obj)
+
+    print(sorted(order_list,reverse=1))
+
+print(ordered_ints([1, True, '123', False, 6, ()]))
+#
+#
+# # 25P - (do not rush to resolve this)
+# # For recursive functions try reading the articles below if you find need more information
+# # https://realpython.com/python-thinking-recursively/
+# # https://www.python-course.eu/python3_recursive_functions.php
+# # After reading the above articles try creating a function to calculate the series (1^2)+(2^2)+(3^2)...(n^2)
+# # The function will receive an int that indicate the number of iterations, or how many times we have (x^2)+
+# # when resolving try using this logic: 1^2+2^2 is 1^2+(1^2+1^2)^2
+#
+# def sum_of_square(n: int):
+#     pass
+#     # <your code here>
+#
+#
+# print(sum_of_square(10))
+#
+#
+# # 25P
+# # Write a function that will calculate factorial of numbers squared.
+# # # For n = 3 the function should calculate (1^2)*(2^2)*(3^2)
+
+def factorial_of_squares(n: int):
+    if n==0:
+        return 1
+    else:
+        resoult=n*factorial_of_squares(n-1)
+        print(resoult)
+
+
+print(factorial_of_squares(5))
 # # #
-# # # 25P
-# # # Write a function that takes in a string as argument and returns a tuple after performing the following actions:
-# # # - the string is split after the first encountered space.
-# # # - all letters in the first half will be transformed to upper case letters
-# # # - all characters that are not lower-case letters in the second half will be replaced with "_"
-# # # - returned tuple contains the two processed strings
-# # # example: "1234567a Text to te5t" will become ("1234567A", "_ext_to_te_t")
 # #
-# example="1234567a Text to te5t"
-# def transform(example):
-#     x=example.split(" ",maxsplit=1)
-#     word1=x[0]
-#     word=x[1]
-#     tuple_1=tuple([word1.upper()])
-#     for carac in word:
-#         if carac.isupper():
-#             word=word.replace(carac,"_")
-#         elif carac==" ":
-#             word=word.replace(" ","_")
-#         elif carac.isdigit():
-#             word=word.replace(carac,"_")
-#     tuple_2=tuple([word])
-#     print(tuple_1+tuple_2)
+# # 25P
+# # Write a function that takes in a string as argument and returns a tuple after performing the following actions:
+# # - the string is split after the first encountered space.
+# # - all letters in the first half will be transformed to upper case letters
+# # - all characters that are not lower-case letters in the second half will be replaced with "_"
+# # - returned tuple contains the two processed strings
+# # example: "1234567a Text to te5t" will become ("1234567A", "_ext_to_te_t")
 #
-# transform("1234567a Text to te5t")
+example="1234567a Text to te5t"
+def transform(example):
+    x=example.split(" ",maxsplit=1)
+    word1=x[0]
+    word=x[1]
+    tuple_1=tuple([word1.upper()])
+    for carac in word:
+        if carac.isupper():
+            word=word.replace(carac,"_")
+        elif carac==" ":
+            word=word.replace(" ","_")
+        elif carac.isdigit():
+            word=word.replace(carac,"_")
+    tuple_2=tuple([word])
+    print(tuple_1+tuple_2)
+
+transform("1234567a Text to te5t")
 
 
 
---
+++ modul6/homework 6.py
@@ -0,0 +1,89 @@
+"""" Homework 6 - needs to be presented before exam day
+# O fabrica de masini are nevoie de un obiect iterabil care sa contina seriile si loturile masinilor produse in ziua respectiva.
+# Seriile si loturile sunt numere intregi de tip int
+# Un lot este alcatuit din 20 de masini si primele 10 din lot sunt cu volan pe dreapta iar urmatoarele 10 cu volan pe stanga
+# Numerotarea loturilor incepe de la prima masina produsa cu seria 1 si lot 1
+# Ziua de lucru stocata in obiectul contruit de voi poate incepe cu orice numar de serie si numarul de bucati produse in acea zi poate avea orice valoare
+# Obiectul iterat va returna numerele loturilor din care fac parte masinile din acea zi, numerotarea lor a inceput de la prima masina produsa cu seria 0 si lot 0
+# Masinile cu serii pare sunt cu volan pe dreapta iar cele cu serii impare cu volan pe stanga
+# 1)Definire: 40p
+#   - Creati o clasa pentru un obiect iterabil
+#    a) constructorul primeste doua argumente de tip int, seria de inceput si numarul de bucati. ex: (101, 41) 10p
+#    b) obiectul are doua metode: prima returneaza o lista cu seriile cu volan pe dreapta si a doua o lista cu seriile cu volan pe stanga 15p
+#    c) iterator-ul returnat de object va comtine loturile din care fac parte seriile din obiectul curent  15p
+# 2) Executie: 40p
+# - Creati o instanta a clasei de mai sus dand ca argumente: serie_inceput 314, bucati 90. 10p
+# - Iterati obiectul creat si salvati fiecare valoare din iteratie in acelas fisier pe linie noua. 30p
+# 3) Documentatie: 20p
+#    a) adaugati type hints 5p
+#    a) documentati modulul 5p
+#    b) documentati clasele 5p
+#    c) documentati metodele 5p
+"""
+
+
+
+class Car_factory():
+    def __init__(self,date:str):
+        self.date=date
+
+    def __iter__(self):
+        all_cars_series = []
+        for series in all_cars_series:
+            all_cars_series.extend(series)
+
+            return CarsIter(all_cars_series)
+
+
+
+
+
+    def add_cars(self,serie:int,nr_buc:int):
+#adauga serile masinilor si numarul de bucati
+        all_cars_series=[]
+        right_steering = []
+        left_steering = []
+        self.serie=serie
+        self.nr_buc=nr_buc
+
+        for  i in range(nr_buc):
+            all_cars_series.append(serie+i)
+            all_cars_series=all_cars_series
+
+        for x in all_cars_series: #iteram toate serile masinilor si stabilim care sunt cu volan pe dreapta si care
+            if x % 2 ==0:         # sunt cu volan pe stanga
+                right_steering.append(x)
+            else:
+                left_steering.append(x)
+        print("right steering wheel:",right_steering)
+        print("left steering wheel:",left_steering)
+        for i in right_steering:
+            f=open('my_file','w')
+            f.write("right steering wheel:"+str(i)+'\n')
+        for x in left_steering:
+            f = open('my_file', 'w')
+            f.write("left steering wheel:" + str(x) + '\n')
+
+
+
+
+class CarsIter():
+#iteratorul
+    def __init__(self,serie:list):
+        self.serie=series
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if not self.serie:
+            raise StopIteration
+        else:
+            return self.serie.pop(0)
+
+
+
+car1=Car_factory("12.04.2021")
+car1.add_cars(101,30)
+car1.add_cars(314,90)
+
+
+
